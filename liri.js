273c16e405ada72099bd5b1e6393a578df2e612e
Jeff Swearingen <jeffdevelopmentacct@gmail.com>
2016-12-14 09:37:31 -0500

    Spotify code added

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-var twitter = require('twitter');
+var Twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 var keys = require('./keys.js');
@@ -14,13 +14,14 @@ console.log('search ' + search);
 
 switch(command) {
 	case 'my-tweets':
+		console.log('my-tweets called');
 		twitter_search();
 		break;
 	case 'spotify-this-song':
-		spotify_search();
+		spotify_search(search);
 		break;
 	case 'movie-this':
-		omdb_search();
+		omdb_search(search);
 		break;
 	case 'do-what-it-says':
 
@@ -31,14 +32,37 @@ switch(command) {
 }
 
 function twitter_search() {
-//  GET https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=developer_jeff&count=20
+	console.log('twitter_search called');
+	var client = new Twitter({
+		consumer_key: keyArray.consumer_key,
+		consumer_secret: keyArray.consumer_secret,
+		access_token_key: keyArray.access_token_key,
+		access_token_secret: keyArray.access_token_secret
+	});
+	console.log(client);
+	var params = {screen_name: 'developer_jeff'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		console.log(response);
+		if (!error) {
+			console.log(tweets.statuses.text);
+		}
+	});
 }
 
-function spotify_search() {
-  GET 
+function spotify_search(search) {
+	spotify.search({ type: 'track', query: search }, function(err, data) {
+		if (err) {
+			console.log('Error occurred: ' + err);
+			return;
+		}
+		console.log('Name: ' + data.tracks.items[0].name);
+		console.log('Artist: ' + data.tracks.items[0].album.artists[0].name);
+		console.log('Preview: ' + data.tracks.items[0].preview_url);
+		console.log('Album: ' + data.tracks.items[0].album.name);
+	});
 } 
 
-function omdb_search() {
+function omdb_search(search) {
 
 }
 
